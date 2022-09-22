940dea68578e8f31facdd5705ff67bd7f8ca9f61
Jorge Salgado <jaso_98@hotmail.com>
2022-09-22 15:59:50 -0500

    FIXED: reviews in ecommerce views.py

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -8,6 +8,7 @@ def home(request):
     # Brings all products if they are available
     products = Product.objects.all().filter(is_available=True).order_by("created_date")
 
+    reviews = None
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
 
